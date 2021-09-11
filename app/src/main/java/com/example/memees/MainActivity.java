81a0ed58cec55952780d9509ab8d4361566f6d2c
Prasant Poonia <prasantchaudhary51@gmail.com>
2021-09-11 09:04:42 +0530

    Added ImageShare

--- app/src/main/java/com/example/memees/MainActivity.java
+++ app/src/main/java/com/example/memees/MainActivity.java
@@ -2,15 +2,21 @@ package com.example.memees;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -28,9 +34,13 @@ import com.bumptech.glide.request.target.Target;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     String currentimageurl=null;
-
+    ImageView imageView=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
       }
     private void loadmeme()
     {
-        ImageView imageView=findViewById(R.id.memeimageView);
+        imageView=findViewById(R.id.memeimageView);
         // Instantiate the RequestQueue.
         ProgressBar progressBar=findViewById(R.id.progressbar);
         progressBar.setVisibility(View.VISIBLE);
@@ -49,17 +59,19 @@ public class MainActivity extends AppCompatActivity {
         String url ="https://meme-api.herokuapp.com/gimme";
         // Request a string response from the provided URL.
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>()
+                {
                     @Override
-                    public void onResponse(JSONObject response) {
+                    public void onResponse(JSONObject response)
+                    {
                         currentimageurl= null;
                         try {
                             currentimageurl = response.getString("url");
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-                        Glide.with(MainActivity.this).load(currentimageurl).listener(new RequestListener<Drawable>() {
+                        Glide.with(MainActivity.this).load(currentimageurl).listener(new RequestListener<Drawable>()
+                        {
                             @Override
                             public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
                                 progressBar.setVisibility(View.GONE);
@@ -87,10 +99,30 @@ public class MainActivity extends AppCompatActivity {
         loadmeme();
     }
 
+    // Retrieving the url to share
+    private Uri getmageToShare(Bitmap bitmap) {
+        File imagefolder = new File(getCacheDir(), "images");
+        Uri uri = null;
+        try {
+            imagefolder.mkdirs();
+            File file = new File(imagefolder, "shared_image.png");
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            uri = FileProvider.getUriForFile(this, "com.anni.shareimage.fileprovider", file);
+        } catch (Exception e) {
+            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return uri;
+    }
     public void sharememe(View view) {
         Intent intent=new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT,currentimageurl);
+        intent.setType("image/*");
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = bitmapDrawable.getBitmap();
+        Uri uri= getmageToShare(bitmap);
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
         Intent chosser=Intent.createChooser(intent, "Share This Meme Using ...");
         startActivity(chosser);
     }
